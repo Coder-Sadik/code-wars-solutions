d3dba2d181a0530c29f946b370dbc1ae5ba2b675
Sadik Rahman <srahman2702@gmail.com>
2024-10-14 00:23:57 +0600

    solved arrayDiff

---
+++ solutions/arrayDiff.js
@@ -0,0 +1,24 @@
+function arrayDiff(a, b) {
+	return a.filter((element) => !b.includes(element));
+}
+
+console.log(arrayDiff([1, 2, 2, 4, 6, 7, 4, 6, 7, 3], [2, 4, 6, 7]));
+
+// without using filter method
+
+// function arrayDiff(a, b) {
+// 	b.forEach((eleB) => {
+// 		remove(a, eleB);
+// 	});
+// 	return a;
+// }
+
+// function remove(ara, value) {
+// 	ara.forEach((element, index) => {
+// 		if (element === value) {
+// 			ara.splice(index, 1);
+// 			remove(ara, value);
+// 		}
+// 	});
+// 	return ara;
+// }
