6b82be04c1cdf5bf077e33afcb882e6e826ce724
Sadik Rahman <srahman2702@gmail.com>
2024-10-19 17:39:12 +0600

    break camel case solution uploaded

--- solutions/arrayDiff.js
+++ solutions/arrayDiff.js
@@ -1,9 +1,11 @@
+//best practice
 function arrayDiff(a, b) {
 	return a.filter((element) => !b.includes(element));
 }
 
 console.log(arrayDiff([1, 2, 2, 4, 6, 7, 4, 6, 7, 3], [2, 4, 6, 7]));
 
+//my noob solution 😐
 // without using filter method
 
 // function arrayDiff(a, b) {
